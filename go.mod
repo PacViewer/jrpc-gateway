36ff2172d44c4acf96ecf5653abe087dd3d61f25
Javad <ja7ad@live.com>
2025-04-23 15:45:16 +0330

    feat: Enable CORS configuration for jrpc server

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- jrpc/options_test.go
+++ jrpc/options_test.go
@@ -1,7 +1,9 @@
 package jrpc
 
 import (
+	"github.com/rs/cors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
@@ -9,9 +11,20 @@ import (
 func TestOptions(t *testing.T) {
 	dur := 5 * time.Second
 
+	corsHeaders := []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token"}
+	corsOrigins := []string{"*"}
+	corsMethods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+
 	opts := []Option{
 		WithCustomHeaders("foobar"),
 		WithReadHeaderTimeout(dur),
+		WithCorsOrigins(&cors.Options{
+			AllowedOrigins:      corsOrigins,
+			AllowedMethods:      corsMethods,
+			AllowedHeaders:      corsHeaders,
+			AllowCredentials:    true,
+			AllowPrivateNetwork: true,
+		}),
 	}
 
 	def := defaultOpt()
@@ -22,4 +35,13 @@ func TestOptions(t *testing.T) {
 
 	assert.Len(t, def.CustomHeadersKey, 2)
 	assert.Equal(t, def.ReadHeaderTimeout.String(), dur.String())
+	require.NotNil(t, def.CorsOptions)
+	assert.Len(t, def.CorsOptions.AllowedOrigins, 1)
+	assert.Equal(t, "*", def.CorsOptions.AllowedOrigins[0])
+	assert.Len(t, def.CorsOptions.AllowedMethods, 5)
+	assert.Equal(t, corsMethods, def.CorsOptions.AllowedMethods)
+	assert.Len(t, def.CorsOptions.AllowedHeaders, 5)
+	assert.Equal(t, corsHeaders, def.CorsOptions.AllowedHeaders)
+	assert.Equal(t, def.CorsOptions.AllowCredentials, true)
+	assert.Equal(t, def.CorsOptions.AllowPrivateNetwork, true)
 }
